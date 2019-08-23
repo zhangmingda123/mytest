568e6c085a48d652d84fe6aca012f37ff1553b8f
zhangmingda123 <1255189729@qq.com>
2019-08-23 19:52:38 +0800

    第四次提交代码

--- src/com/demo/Demo.java
+++ src/com/demo/Demo.java
@@ -3,7 +3,7 @@ package com.demo;
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("ÄãºÃ");
+		System.out.println("haha");
 		System.out.println("world");
 	}
 
